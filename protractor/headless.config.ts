71bbd253f7dcf73e9f342d44cca0ded83d81e2a9
manuelam20 <42817076+manuelam20@users.noreply.github.com>
2019-01-25 08:23:40 -0500

    setting up the debuger and lint (#6)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,16 +3,16 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/google.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
   capabilities: {
-	browserName: 'chrome',
-	chromeOptions: {
-		args: ['--headless', '--disable-gpu']
-	}
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
-	reporter();
+    reporter();
   }
 };
